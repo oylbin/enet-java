b2e804fda2309cdbc85d6e2e5de4d64b52f4500d
oylbin <oylbin@gmail.com>
2024-03-11 17:43:31 +0800

    fix spotbugs warnings

--- java/com/example/EnetTest.java
+++ java/com/example/EnetTest.java
@@ -20,9 +20,9 @@ public class EnetTest {
     }
     public static void main(String[] args) throws UnknownHostException {
         System.out.println("hello, world!");
-        System.out.printf("enet version: %d.%d.%d\n", enet.ENET_VERSION_MAJOR, enet.ENET_VERSION_MINOR, enet.ENET_VERSION_PATCH);
+        System.out.printf("enet version: %d.%d.%d%n", enet.ENET_VERSION_MAJOR, enet.ENET_VERSION_MINOR, enet.ENET_VERSION_PATCH);
         int ret = enet.enet_initialize();
-        System.out.printf("enet initialize: %d\n", ret);
+        System.out.printf("enet initialize: %d%n", ret);
         ENetAddress address = new ENetAddress();
         // convert ip address string to long
         String IPString = "127.0.0.1";
@@ -35,7 +35,7 @@ public class EnetTest {
         address.setHost(0);
         address.setPort(5678);
         long max_clients = 32;
-        System.out.printf("host[%d], port[%d]\n", address.getHost(), address.getPort());
+        System.out.printf("host[%d], port[%d]%n", address.getHost(), address.getPort());
         // wait for user input
         /*
         System.out.println("Press any key to continue...");
@@ -50,24 +50,24 @@ public class EnetTest {
             System.out.println("enet host create failed");
             return;
         }
-        System.out.printf("enet host create: %d, port[%d]\n", ret, host.getAddress().getPort());
+        System.out.printf("enet host create: %d, port[%d]%n", ret, host.getAddress().getPort());
         ENetEvent event = new ENetEvent();
 
 
         while(true){
             ret = enet.enet_host_service(host, event, 5000);
-            System.out.printf("enet host service: %d\n", ret);
+            System.out.printf("enet host service: %d%n", ret);
             ENetEventType t = event.getType();
             if(t.equals(ENetEventType.ENET_EVENT_TYPE_CONNECT)){
                 String name = "client"+event.getPeer().getIncomingPeerID();
-                System.out.printf("%s connected\n", name);
+                System.out.printf("%s connected%n", name);
                 String message = "hello, " + name;
-                ENetPacket p = enet.enet_packet_create(message.getBytes(), enetJNI.ENET_PACKET_FLAG_RELIABLE_get());
+                ENetPacket p = enet.enet_packet_create(message.getBytes(Charset.forName("utf8")), enetJNI.ENET_PACKET_FLAG_RELIABLE_get());
                 enet.enet_peer_send(event.getPeer(), (short)0, p);
             }else if(t.equals(ENetEventType.ENET_EVENT_TYPE_RECEIVE)){
                 ENetPacket packet = event.getPacket();
                 String name = "client"+event.getPeer().getIncomingPeerID();
-                System.out.printf("received %d bytes from %s\n", packet.getDataLength(), name);
+                System.out.printf("received %d bytes from %s%n", packet.getDataLength(), name);
                 // packet->data is ignored in src/swag.i
                 //          %ignore _ENetPacket::data;
                 // remove this line does not work. I don't know how to resolve it.
@@ -80,18 +80,18 @@ public class EnetTest {
 
                 byte[] bytes = new byte[(int) packet.getDataLength()];
                 enet.enet_get_packet_data(packet, bytes);
-                System.out.println("string = " + new String(bytes, Charset.defaultCharset()));
+                System.out.println("string = " + new String(bytes, Charset.forName("utf8")));
                 String msg = "server received "+ packet.getDataLength() + " bytes from you.";
-                ENetPacket p = enet.enet_packet_create(msg.getBytes(), enetJNI.ENET_PACKET_FLAG_RELIABLE_get());
+                ENetPacket p = enet.enet_packet_create(msg.getBytes(Charset.forName("utf8")), enetJNI.ENET_PACKET_FLAG_RELIABLE_get());
                 enet.enet_peer_send(event.getPeer(), (short)0, p);
 
                 // send a packet to all clients
-                String broadcastMsg = name + ": " + new String(bytes, Charset.defaultCharset());
-                ENetPacket p1 = enet.enet_packet_create(broadcastMsg.getBytes(), enetJNI.ENET_PACKET_FLAG_RELIABLE_get());
+                String broadcastMsg = name + ": " + new String(bytes, Charset.forName("utf8"));
+                ENetPacket p1 = enet.enet_packet_create(broadcastMsg.getBytes(Charset.forName("utf8")), enetJNI.ENET_PACKET_FLAG_RELIABLE_get());
                 enet.enet_host_broadcast(host, (short)0, p1);
             }else if(t.equals(ENetEventType.ENET_EVENT_TYPE_DISCONNECT)){
                 String name = "client"+event.getPeer().getIncomingPeerID();
-                System.out.printf("%s disconnected\n", name);
+                System.out.printf("%s disconnected%n", name);
             }
         }
     }
--- java/com/example/EnetTestClient.java
+++ java/com/example/EnetTestClient.java
@@ -21,21 +21,22 @@ public class EnetTestClient {
     }
     public static void main(String[] args) throws UnknownHostException {
         System.out.println("hello, world!");
-        System.out.printf("enet version: %d.%d.%d\n", enet.ENET_VERSION_MAJOR, enet.ENET_VERSION_MINOR, enet.ENET_VERSION_PATCH);
+        System.out.printf("enet version: %d.%d.%d%n", enet.ENET_VERSION_MAJOR, enet.ENET_VERSION_MINOR, enet.ENET_VERSION_PATCH);
         int ret = enet.enet_initialize();
-        System.out.printf("enet initialize: %d\n", ret);
+        System.out.printf("enet initialize: %d%n", ret);
         ENetAddress address = new ENetAddress();
         enet.enet_address_set_host(address, "127.0.0.1");
         address.setPort(5678);
-        System.out.printf("host[%d], port[%d]\n", address.getHost(), address.getPort());
+        System.out.printf("host[%d], port[%d]%n", address.getHost(), address.getPort());
 
-        ENetAddress address2 = new ENetAddress();
-        ENetHost host = enet.enet_host_create(address2 ,1, 2, 0, 0);
+        // when creating enet client, the C API need first parameter to be null
+        // see http://enet.bespin.org/Tutorial.html
+        ENetHost host = enet.enet_host_create(null ,1, 2, 0, 0);
         if (host == null) {
             System.out.println("enet host create failed");
             return;
         }
-        System.out.printf("enet host create: %d, port[%d]\n", ret, host.getAddress().getPort());
+        System.out.printf("enet host create: %d, port[%d]%n", ret, host.getAddress().getPort());
 
         ENetPeer peer = enet.enet_host_connect(host, address, 2, 0);
         if (peer == null) {
@@ -48,7 +49,7 @@ public class EnetTestClient {
 
         int loopCount = 0;
         while(true){
-            System.out.printf("loopCount[%d]\n", loopCount++);
+            System.out.printf("loopCount[%d]%n", loopCount++);
 
             ret = enet.enet_host_service(host, event, 0);
             while ( ret > 0){
@@ -57,21 +58,21 @@ public class EnetTestClient {
                     System.out.println("Connected to server");
                 }else if(t.equals(ENetEventType.ENET_EVENT_TYPE_RECEIVE)){
                     ENetPacket packet = event.getPacket();
-                    System.out.printf("received %d bytes from server\n", packet.getDataLength());
+                    System.out.printf("received %d bytes from server%n", packet.getDataLength());
 
                     byte[] bytes = new byte[(int) packet.getDataLength()];
                     enet.enet_get_packet_data(packet, bytes);
-                    System.out.println("received string = " + new String(bytes, Charset.defaultCharset()));
+                    System.out.println("received string = " + new String(bytes, Charset.forName("utf8")));
 
                 }else if(t.equals(ENetEventType.ENET_EVENT_TYPE_DISCONNECT)){
                     String name = "server"+event.getPeer().getIncomingPeerID();
-                    System.out.printf("%s disconnected\n", name);
+                    System.out.printf("%s disconnected%n", name);
                 }
                 ret = enet.enet_host_service(host, event, 0);
             }
             if (loopCount % 5 == 0){
                 String msg = "current time : " + System.currentTimeMillis();
-                ENetPacket p = enet.enet_packet_create(msg.getBytes(), enetJNI.ENET_PACKET_FLAG_RELIABLE_get());
+                ENetPacket p = enet.enet_packet_create(msg.getBytes(Charset.forName("utf8")), enetJNI.ENET_PACKET_FLAG_RELIABLE_get());
                 enet.enet_peer_send(peer, (short)0, p);
             }
 
